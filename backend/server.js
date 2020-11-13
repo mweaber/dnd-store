d732fda8bde82566befc6135f8c2d0c60a459727
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-13 12:38:15 -0500

    mongo setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
 
+
+dotenv.config()
 const app = express()
 
-app.listen(5000, console.log('Server running on port 5000'))
 
 app.get('/', (req, res) => {
     res.send('API is running')
@@ -17,3 +19,6 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
