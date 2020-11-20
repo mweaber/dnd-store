76be191a374e9cc2276dae813f5b90a25979e52a
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-19 21:28:42 -0500

    user auth backend working, register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 //@desc     Auth user & get token
 //@route    POST /api/users/login
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -23,6 +24,61 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+//@desc     Register a new user
+//@route    POST /api/users
+//@access   Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, password, name } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User Already Exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+//@desc     Get user profile
+//@route    GET /api/users/profile
+//@access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(401)
+        throw new Error('User not found')
+    }
+})
+
+
+
 export {
-    authUser
+    authUser,
+    getUserProfile,
+    registerUser
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,30 +3,39 @@ import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         required: true
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true
     },
     password: {
-        type:String,
+        type: String,
         required: true
     },
     isAdmin: {
-        type:Boolean,
+        type: Boolean,
         required: true,
         default: false
     }
-},{
+}, {
     timestamps: true
 })
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
