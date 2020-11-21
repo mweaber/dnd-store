39e7cec0e30c19b1e8c3e82215b506eaaa32d5a6
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-21 15:49:30 -0500

    shipping address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -18,10 +18,11 @@ const reducer = combineReducers({
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+const shippingAddresFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
-    userLogin: {userInfo: userInfoFromStorage}
+    cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddresFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 const middleware = [thunk]
 
