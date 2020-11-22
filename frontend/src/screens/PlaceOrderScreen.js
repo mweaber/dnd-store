69d545025789712562087470193f1332959ce51b
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-22 15:43:05 -0500

    backend order saving

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Row, Col, ListGroup, Image, Card, ListGroupItem } from 'react-bootstrap'
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
     const cart = useSelector(state => state.cart)
+    const dispatch = useDispatch()
 
     // Calculate Prices
     const addDecimals = (num) => {
@@ -24,13 +26,29 @@ const PlaceOrderScreen = () => {
     cart.totalPrice = (
         Number(cart.itemsPrice) +
         Number(cart.shippingPrice) +
-        Number(cart.taxPrice) 
-        ).toFixed(2)
+        Number(cart.taxPrice)
+    ).toFixed(2)
 
+    const orderCreate = useSelector(state => state.orderCreate)
+    const { order, success, error } = orderCreate
 
+    useEffect(() => {
+        if (success) {
+            history.push(`/orders/${order._id}`)
+        }
+        //eslint-disable-next-line
+    }, [history, success])
 
     const placeOrderHandler = () => {
-        console.log('Place Order');
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice
+        }))
     }
 
     return (
@@ -127,7 +145,9 @@ const PlaceOrderScreen = () => {
                                         ${cart.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
-
+                            <ListGroup.Item>
+                                {/* {error && <Message variant='danger'>{error}</Message>} */}
+                            </ListGroup.Item>
                             <ListGroup.Item>
                                 <Button type='button' className='btn-block' disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
                                     Place Order
