eb87a621a6ea003ce09d3c77d482d680040de0de
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-27 14:44:56 -0500

    admin backend routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
-router.route('/').post(registerUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile)
 
 export default router
