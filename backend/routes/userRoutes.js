2cd2a978c4cfe6ba888733814c7d449a55222ccc
Matthew Weaber <matthewweaber89@gmail.com>
2020-11-28 14:02:43 -0500

    get user by id and edit working

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,22 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
-router.route('/').post(registerUser).get(protect, admin, getUsers)
+router
+    .route('/')
+    .get(protect, admin, getUsers)
+    .post(registerUser)
 router
     .route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile)
 
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+    .route('/:id')
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser)
+    .delete(protect, admin, deleteUser)
 
 export default router
