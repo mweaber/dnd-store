27aa5f8d1913dbf261059a1bacbf70218683eeca
Matthew Weaber <matthewweaber89@gmail.com>
2021-01-04 16:06:18 -0500

    reviews frontend working

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,8 @@ import {
     productDetailsReducer,
     productDeleteReducer,
     productCreateReducer,
-    productUpdateReducer
+    productUpdateReducer,
+    productReviewCreateReducer
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -34,6 +35,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
